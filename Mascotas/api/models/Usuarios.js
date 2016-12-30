52851bb0901e8b81f9e111b63de25ba209f0c160
Audita <melrose.70@hotmail.com>
2016-12-29 22:01:36 -0500

    model

--- Mascotas/api/models/Usuarios.js
+++ Mascotas/api/models/Usuarios.js
@@ -8,7 +8,20 @@
 module.exports = {
 
   attributes: {
-
+      nombres:{
+          type:'string',
+          minLength:5
+      },
+      apellidos:{
+          type:'string',
+          minLength:5
+      },
+      correo:{
+          type:'string',
+          email:true,
+          defaultsTo:'correo@invalido.com'
+      }
   }
+    
 };
 
