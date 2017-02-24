5f9359173de8473977f04395973db8d359ff1ee9
Audita <melrose.70@hotmail.com>
2017-02-24 09:26:31 -0500

    formulario

--- Angular2/prueba/src/app/app.component.ts
+++ Angular2/prueba/src/app/app.component.ts
@@ -1,44 +1,60 @@
 import {Component, OnInit} from "@angular/core";
+import {Response, Http} from "@angular/http";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-//ctrl + alt + l
-export class AppComponent implements OnInit{
-title:string = "Hola amigos";
-  apellido:string="";
-  nombre:string="";
-  colorH4 ="red";
-  tamanoH4 = "25px";
+// CTRL A +  -  CTRL + ALT + L
+export class AppComponent implements OnInit {
+  title: string = "Hola Amigos";
+  nombre: string = "";
+  apellido: string = "";
+  colorH4 = "red";
+  tamanoH4 = "52px";
+  classes = "btn btn-block btn-success";
 
   nuevaTienda:any={};
 
 
 
-   constructor(){
-     this.apellido="Quito";
-     this.nombre="Reyes";
-     console.log("Inicio el constructor");
+  constructor(private http: Http) {
+    this.apellido = "Eguez";
+    this.nombre = "Adrian";
+    console.log("Inicio el construcor")
   }
 
-  ngOnInit(){
-    this.apellido="Reyes";
-    this.nombre="Jelena";
+  ngOnInit() {
+    this.apellido = "Sarzosa";
+    this.nombre = "Vicente";
+    console.log("On Init")
   }
 
-  nombreCompleto():string{
+  nombreCompleto(): string {
     return `${this.nombre} ${this.apellido}`
   }
 
-  hizoClick(){
-  console.log("hizoClick");
-}
-  hizoFocus(){
-    console.log("hizoFocus");
+  hizoClick() {
+    console.log("Hizo Click");
+    console.log()
+  }
+
+  hizoFocus() {
+    console.log("Hizo focus");
+  }
+
+
+  crearTienda(formulario){
+    console.log(formulario);
+    this.http
+      .post("http://localhost:1337/Tienda", formulario.valores)
+      .subscribe(
+        res=>console.log('Respuesta: ',res),
+        err=>console.log('Error: ',err),
+        ()=>{
+          console.log("Se completo la accion")
+        }
+      );
   }
 }
-
-
-
-
