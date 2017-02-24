cba5ec573ab95a0581e4d58c07fa9798c19daf36
Audita <melrose.70@hotmail.com>
2017-02-24 17:14:08 -0500

    producto

--- Angular2/prueba/src/app/app.component.ts
+++ Angular2/prueba/src/app/app.component.ts
@@ -11,65 +11,20 @@ import {MasterURLService} from "./services/master-url.service";
 // CTRL A +  -  CTRL + ALT + L
 export class AppComponent implements OnInit {
   title: string = "Bienvenido a Ingresar Tiendas";
-  nuevaTienda= {};
+  nuevaTienda = {};
   tiendas = [];
   disabledButtons = {
-    NuevaTiendaFormSubmitButton:false
+    NuevaTiendaFormSubmitButton: false
   };
+
   constructor(private _http: Http,
               private _masterURL: MasterURLService) {
   }
+
   ngOnInit() {
-    this._http.get(this._masterURL.url+"Tienda")
 
-      .subscribe(
-        (res:Response)=>{
-          this.tiendas = res.json();
-        },
-        (err)=>{
-          console.log(err);
-        }
-      )
-  }
-  crearTienda(formulario:NgForm) {
-    console.log(formulario);
-    this.disabledButtons.NuevaTiendaFormSubmitButton = true;
-    this._http.post(this._masterURL.url+"Tienda", {
-      nombre:formulario.value.nombre
-    }).subscribe(
-      (res)=>{
-        console.log("No hubo Errores");
-        console.log(res);
-        this.nuevaTienda = {};
-        this.disabledButtons.NuevaTiendaFormSubmitButton = false;
-      },
-      (err)=>{
-        this.disabledButtons.NuevaTiendaFormSubmitButton = false;
-        console.log("Ocurrio un err or",err);
-      },
-      ()=>{
-        console.log("Termino la función vamos a las casas")
-      }
-    );
-
-  }
-
-  borrarTienda(id:number){
-  this._http.delete(this._masterURL.url+"Tienda/"+id)
-    .subscribe(
-      (res)=>{
-        let tiendaBorrada = res.json();
-        this.tiendas =this.tiendas.filter(value=>tiendaBorrada.id=value.id)
-      },
-      (err)=>{
-        console.log(err);
-      }
-
-    )
   }
 
 
 
 }
-
-
